85b663f4afbb96a0838f3ec9c06b12136e38947d
user <user@DESKTOP-BTK7FO7>
2019-09-03 10:44:54 +0900

    UserMailSendService.java 내용 추가했어요..ㅎ

--- StudyPlanner/src/main/java/com/ncs/green/UserMailSendService.java
+++ StudyPlanner/src/main/java/com/ncs/green/UserMailSendService.java
@@ -39,4 +39,34 @@ public class UserMailSendService {
 		}
 		//요기는 저희도 긁어온거라...정확히는 모르겠어욤
 	}
+	
+	
+	// [2ndrow:left] ::임시비번메시지 발송----------------------------------------------┐
+	public void findPasswordTemporary(String id, String name, String password, HttpServletRequest request) {  //id,name,난수(임시비번)를 받아온다
+		//1.난수생성(대문자+소문자+숫자+특수문자 10글자 랜덤)함과 동시에 데이터베이스 비번 업데이트해야함.
+		//	대문자+소문자+숫자+특수문자 20글자 랜덤
+		//	SELECT DBMS_RANDOM.STRING('P', 10) STR FROM DUAL; 
+		//		출처: https://oracle.tistory.com/400 [안나푸르나]
+		System.out.println("null 값이 왜 나오는지...???");
+		System.out.println("id는 "+id+"  ,password는 "+password+"  ,  name은  "+name+"  입니다.");
+		//2.위의 1번에 관련된 쿼리문(mapper) 작성해야함.
+		//3.쿼리문에 접근 및 실행하는 코드 작성.
+		//4.쿼리문 접금 및 실행 후 생성된 난수 변수에 담아 아래의 코드중 삽입
+		MimeMessage mail = mailSender.createMimeMessage();
+		//tp=이곳에 난수생성된후 업데이트된 비밀번호 받아옴
+		String htmlStr = "<h2>안녕하세요 2nd row 스터디플레너입니다!</h2><br><br>" 
+				+ "<h3> 임시비밀번호가 요청에 의한 메일입니다.</h3>" 
+				 
+				+ "임시비밀번호는 "+ password+"입니다. 홈페이지에서 로그인 한 후 비밀번호를 수정해 주세요.";  
+		try {
+			mail.setSubject("[2nd row] 스터디 플레너가 발송한 인증메일입니다", "utf-8");
+			mail.setText(htmlStr, "utf-8", "html");
+			mail.addRecipient(RecipientType.TO, new InternetAddress(id));
+			mailSender.send(mail);
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//---------------------------------------------------------------------------┘
 }
