c08a97d2f1ebfbce054f4b8cde5b53ef453333ac
user <user@DESKTOP-BTK7FO7>
2019-09-03 10:05:16 +0900

    1.[2nd row::left] business폴더 =>StudentService.java 에 내용 추가..4개의 메소드..

--- StudyPlanner/src/main/java/business/StudentService.java
+++ StudyPlanner/src/main/java/business/StudentService.java
@@ -19,5 +19,15 @@ public interface StudentService {
 	int updateConfirm(StudentVO vo);
 
 	StudentVO selectSeq(StudentVO vo);
- 
+
+	// 로그인 및 비번찾기 --------------------------------------------
+	StudentVO loginCheck(StudentVO vo); // 로그인
+
+	StudentVO selectPassword(StudentVO vo); // 패스워드계정찾는 ..
+
+	int updateTemPassword(StudentVO vo); // 임시비번으로 변경
+
+	StudentVO selectAfterPW(StudentVO vo); //임시비번으로 변경된 거 조회
+	//---------------------------------------------------------┘
+
 }
